ecba5ae0532be9352af44d9879177a21c347320e
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2025-01-08 10:38:16 -0400

    fixing file shell_getline_command.c

--- shell_getline_command.c
+++ shell_getline_command.c
@@ -7,17 +7,17 @@
 
 char *getline_command(void)
 {
-        char *lineptr = NULL;
-        size_t charter_user = 0;
+	char *lineptr = NULL;
+	size_t charter_user = 0;
 
-        if (isatty(STDIN_FILENO))
-                writes(STDOUT_FILENO, "$ ", 2);
+	if (isatty(STDIN_FILENO))
+		writes(STDOUT_FILENO, "$ ", 2);
 
-        if (getline(&lineptr, &charter_user, stdin) == -1)
-        {
-                free(lineptr);
-                return (NULL);
-        }
+	if (getline(&lineptr, &charter_user, stdin) == -1)
+	{
+		free(lineptr);
+		return (NULL);
+	}
 
-        return (lineptr);
+	return (lineptr);
 }
