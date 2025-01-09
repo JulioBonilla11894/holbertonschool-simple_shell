78a2c74396b78a5d159987f73c618418e1973a02
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2025-01-09 12:23:13 -0400

    fixing file shell.h

--- shell.h
+++ shell.h
@@ -9,28 +9,23 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <ctype.h>
 
-
-int lsh_ctrld(char **args);
-int lsh_cd(char **args);
-int lsh_help(char **args);
-extern char **environ;
-int lsh_exit(char **args);
-int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
+int process_builtins(char **commands, char **env);
 int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _putchar(char c);
-
-char *_get_path(char ** env);
-int _values_path(char **arg, char **env);
+int _strcmp(char *s1, char *s2);
 char *_getline_command(void);
+char **tokenize(char *line);
 void _getenv(char **env);
-char **tokenize(char *lineptr);
-void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
-char *lineptr, int np, int c);
+int _values_path(char **command, char **env);
+int _fork_fun(
+	char **commands,
+	char **av,
+	char **env,
+	char *line,
+	int pathValue,
+	int is_path);
 
-
-#endif /* SHELL_H */
+#endif
