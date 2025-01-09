ab0f4b55734269202d22ac1d5753a22ce736d1e8
Yoruan <yoruanorta50@gmail.com>
2025-01-09 11:59:50 -0400

    Test if it work the shell

---
+++ main.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * main - Main function of the shell
+ * @ac: Argument count (unused)
+ * @av: Argument vector (unused)
+ * @env: Environment variables
+ * Return: Shell exit status
+ */
+int main(int ac, char **av, char **env)
+{
+	char *line = NULL;
+	char **commands = NULL;
+	int pathValue = 0, status = 0, is_path = 0;
+
+	(void)ac;
+	while (1)
+	{
+		line = _getline_command();
+		if (!line)
+		return (0);
+
+		commands = tokenize(line);
+		if (!commands)
+		{
+			free(line);
+			continue;
+		}
+
+		pathValue++;
+		is_path = _values_path(&commands[0], env);
+		status = process_builtins(commands, env);
+		if (status == 1)
+		{
+			free(commands);
+			free(line);
+			continue;
+		}
+
+		if (is_path == 0)
+		status = _fork_fun(commands, av, env, line, pathValue, is_path);
+
+		if (is_path == 0)
+		free(commands[0]);
+
+		free(commands);
+		free(line);
+	}
+
+	return (status);
+}
+
+/**
+ * process_builtins - Handle built-in commands (exit, env)
+ * @commands: Tokenized user input
+ * @env: Environment variables
+ * Return: 1 if a built-in command is processed, otherwise 0
+ */
+int process_builtins(char **commands, char **env)
+{
+	if (!_strcmp(commands[0], "exit"))
+	{
+		free(commands[0]);
+		free(commands);
+		return (1);
+	}
+	else if (!_strcmp(commands[0], "env"))
+	{
+		_getenv(env);
+		free(commands);
+		return (1);
+	}
+	return (0);
+}
+
---
+++ utils.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+	len++;
+	return (len);
+}
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: String 1
+ * @s2: String 2
+ * Return: 0 if equal, non-zero otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * _getline_command - Custom function to read input from user
+ * Return: Input string
+ */
+char *_getline_command(void)
+{
+	size_t bufsize = 0;
+	char *line = NULL;
+
+	getline(&line, &bufsize, stdin);
+	return (line);
+}
+
+/**
+ * tokenize - Tokenizes the input string into commands
+ * @line: Input string to tokenize
+ * Return: Array of strings (commands)
+ */
+char **tokenize(char *line)
+{
+	char **tokens = malloc(sizeof(char *) * 64);
+	int index = 0;
+	char *token = strtok(line, " \t\r\n\a");
+
+	while (token != NULL)
+	{
+		tokens[index] = token;
+		index++;
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[index] = NULL;
+	return (tokens);
+}
