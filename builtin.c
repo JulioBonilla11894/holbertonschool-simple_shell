dce7cbcd529ff4787c26ca9c130b97ffc9058842
Yoruan <yoruanorta50@gmail.com>
2025-01-10 18:22:11 -0400

    Final test of the project

--- builtin.c
+++ builtin.c
@@ -1,34 +1,53 @@
 #include "shell.h"
-
+/**
+ * process_builtins - Checks if a command is a built-in command and processes.
+ * @commands: An array of strings containing the tokenized user input.
+ * @env: The environment variables array.
+ *
+ * Return: 1 if a built-in command was processed, 0 otherwise.
+ */
 int process_builtins(char **commands, char **env)
 {
-    if (!_strcmp(commands[0], "exit"))
-    {
-        exit(0);
-    }
-    else if (!_strcmp(commands[0], "env"))
-    {
-        _getenv(env);
-        return (1);
-    }
-    return (0);
+	if (!_strcmp(commands[0], "exit"))
+	{
+		exit(0);
+	}
+	else if (!_strcmp(commands[0], "env"))
+	{
+		_getenv(env);
+		return (1);
+	}
+	return (0);
 }
 
+/**
+ * _getenv - Prints the environment variables.
+ * @env: The environment variables array.
+ */
 void _getenv(char **env)
 {
-    int i;
-    for (i = 0; env[i]; i++)
-    {
-        printf("%s\n", env[i]);
-    }
+	int i;
+
+	for (i = 0; env[i]; i++)
+	{
+		printf("%s\n", env[i]);
+	}
 }
 
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 0 if the strings are equal, a positive integer if s1 > s2,
+ *         and a negative integer if s1 < s2.
+ */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
--- main.c
+++ main.c
@@ -1,49 +1,51 @@
 #include "shell.h"
-
+/**
+ * main - Entry point for the shell program.
+ * @ac: Argument count (unused in this function).
+ * @av: Argument vector (unused in this function).
+ * @env: Array of environment variables.
+ *
+ * Return: Always returns 0 on successful exit.
+ */
 int main(int ac, char **av, char **env)
 {
-    char *line = NULL;
-    char **commands = NULL;
-    int pathValue = 0, status = 0;
-    int interactive_mode = isatty(STDIN_FILENO);
+	char *line = NULL;
+	char **commands = NULL;
+	int pathValue = 0, status = 0;
+	int interactive_mode = isatty(STDIN_FILENO);
 
-    (void)ac;
-    while (1)
-    {
-        if (interactive_mode)
-        {
-            printf("yoruan_shell: ");
-        }
+	(void)ac;
+	while (1)
+	{
+		if (interactive_mode)
+		{
+			printf("simple_shell: ");
+		}
 
-        line = _getline_command();
-        if (!line)
-            return (0);
-
-        commands = tokenize(line);
-        if (!commands)
-        {
-            free(line);
-            continue;
-        }
-
-        pathValue++;
-        if (access(commands[0], X_OK) == 0)
-        {
-            status = _fork_fun(commands, av, env, line, pathValue, 1);
-        }
-        else
-        {
-            status = process_builtins(commands, env);
-            if (status == 0)
-                fprintf(stderr, "%s: Command not found\n", commands[0]);
-        }
-
-        free(commands);
-        free(line);
-
-        if (!interactive_mode && feof(stdin))
-            break;
-    }
-
-    return (0);
+		line = _getline_command();
+		if (!line)
+		return (0);
+		commands = tokenize(line);
+		if (!commands)
+		{
+			free(line);
+			continue;
+		}
+		pathValue++;
+		if (access(commands[0], X_OK) == 0)
+		{
+			status = _fork_fun(commands, av, env, line, pathValue, 1);
+		}
+		else
+		{
+			status = process_builtins(commands, env);
+			if (status == 0)
+			fprintf(stderr, "%s: Command not found\n", commands[0]);
+		}
+		free(commands);
+		free(line);
+		if (!interactive_mode && feof(stdin))
+		break;
+	}
+	return (0);
 }
